b9c3492dcaaf124bc7ec904fb08a309ebefa19d8
Tigran <tigrankoshkelyan@gmail.com>
2023-03-08 22:47:51 +0300

    Builds

--- karma/src/specs/syntax.cpp
+++ karma/src/specs/syntax.cpp
@@ -2,9 +2,13 @@
 
 #include <cctype>
 
+namespace karma::detail::specs::syntax {
+
 bool IsAllowedLabelChar(char symbol) {
     return std::islower(symbol) != 0 ||  //
            std::isdigit(symbol) != 0 ||  //
            symbol == '_' ||              //
            symbol == '.';
 }
+
+}  // namespace karma::detail::specs::syntax
