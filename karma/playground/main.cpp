3364a2b7a34df8bc27b47a72a9a7c7b2f8918200
Tigran <tigrankoshkelyan@gmail.com>
2023-03-21 15:04:55 +0300

    printf play

--- karma/playground/main.cpp
+++ karma/playground/main.cpp
@@ -2,6 +2,13 @@
 #include <karma>
 #include <string>
 
+class NoOpStreamBuff : public std::streambuf {
+   public:
+    int overflow(int c) {
+        return c;
+    }
+};
+
 std::string Assembler(const std::string& filepath) {
     return filepath + ".krm";
 }
@@ -11,16 +18,19 @@ std::string Exec(const std::string& filepath) {
 }
 
 int main() {
-    std::string filepath = "../programs/print/playgrounds/int32";
+    std::string filepath = "../programs/print/playgrounds/printf";
 
-    karma::Compiler::Compile(Assembler(filepath));
+    NoOpStreamBuff no_op_stream_buff;
+    std::ostream no_op_stream(&no_op_stream_buff);
+
+    karma::Compiler::Compile(Assembler(filepath), "", no_op_stream);
 
     karma::Executor executor;
-    uint32_t ret_code = executor.Execute(Exec(filepath));
+    uint32_t ret_code = executor.Execute(Exec(filepath), no_op_stream);
 
     std::cout << "executable returned code " << ret_code << std::endl;
 
-    karma::Disassembler::Disassemble(Exec(filepath));
+    karma::Disassembler::Disassemble(Exec(filepath), "", no_op_stream);
 
     return 0;
 }
